2a98c6b9135deecc6cb1ce241c46ad98042ba437
Annisa Aprilia Lestari <annisaaprilialestari05@gmail.com>
2025-12-08 09:55:26 +0700

    Add files via upload

--- tugas_3/index.php
+++ tugas_3/index.php
@@ -1,1 +1,22 @@
+<?php
+$status = $_GET['status'] ?? '';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Halaman Utama Pendaftaran</title>
+</head>
+<body>
 
+<h1>Halaman Utama Pendaftaran Siswa Baru</h1>
+
+<?php if ($status === 'sukses'): ?>
+    <p style="color: green;">Pendaftaran berhasil disimpan.</p>
+<?php elseif ($status === 'gagal'): ?>
+    <p style="color: red;">Pendaftaran gagal disimpan.</p>
+<?php endif; ?>
+
+<p><a href="form-daftar.php">Kembali ke Form Pendaftaran</a></p>
+
+</body>
+</html>
